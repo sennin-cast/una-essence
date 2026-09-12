9f02dae34b26151f7830651d9da6e1c85eb4c323
sennin-cast <sennincast@gmail.com>
2026-09-12 18:38:14 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let basePath = '';
-
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repoName = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  // Se o repositório for nome-usuario.github.io, roda na raiz. Senão, adiciona o /nome-do-repo
-  if (!repoName.includes('.github.io')) {
-    basePath = `/${repoName}`;
-  }
-}
-
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/una-essence',
   images: {
     unoptimized: true,
   },
-  basePath: basePath || undefined,
-  assetPrefix: basePath || undefined,
 };
 
 export default nextConfig;
